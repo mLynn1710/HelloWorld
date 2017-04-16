c256a905ebe612a5c91c6057e48eedae1daa016a
M. MLynn <mlynn1710@gmail.com>
2017-04-16 15:40:03 +0800

    add dialog for exit

--- app/src/main/java/marlyn/helloworld/MainActivity.java
+++ app/src/main/java/marlyn/helloworld/MainActivity.java
@@ -1,13 +1,50 @@
 package marlyn.helloworld;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnlogout = (Button) findViewById(R.id.btn_logout);
+        btnlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Info");
+                builder.setMessage("Do you want to logout ??");
+                builder.setPositiveButton("Yes. Take me away!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        finish();
+
+                    }
+                });
+
+                builder.setNegativeButton("Not now", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        dialogInterface.dismiss();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
     }
 }
